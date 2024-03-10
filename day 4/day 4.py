805459cc249c2d021ec627ced97cdf87ac3f4a0d
jgusho1717 <rezijgushia17@gmail.com>
2024-03-10 15:22:21 +0400

    commit

---
+++ day 4/day 4.py
@@ -0,0 +1,31 @@
+# inputs and outputs
+
+#output
+
+#print(print(rezi))
+#input
+
+#print("please enter your name")
+
+#name = input("please enter your name:")
+#print(name)
+#lastname = input("please enter your lastname")
+#print(lastname)
+#age = input("please enter yor age")
+#print(age)
+
+
+#homework of day 4
+
+#num1 = int(input("please enter your first number:"))
+#num2 = int(input("please enter your second number"))
+
+#print(num1 + num2)
+
+#homework of the day #2
+
+name = input("whats your name")
+age = int(input("whats your age:"))
+future_years = int(input("how many years in to the future do you want to travel in"))
+age_in_future = age + future_years
+print(age_in_future)
