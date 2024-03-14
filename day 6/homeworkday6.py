918d6320d6e6bcfc11fc5796917c8577cc528df2
jgusho1717 <rezijgushia17@gmail.com>
2024-03-15 00:53:23 +0400

    'Davaleba'\

---
+++ day 6/homeworkday6.py
@@ -0,0 +1,67 @@
+# num1 =(input("please enter a number:"))
+# num2 =(input("please enter second number:"))
+
+# print(num1 == num2)
+# homework n1 done
+
+# > operation
+# print(5 >2)
+# print(10 >5)
+# print(9 >6)
+# print(3>2)
+# print(55> 39)
+#> operation 
+
+# >= operation
+# print(15>=10)
+# print(21>=11)
+# print(11>=10)
+# print(13>=13)
+# print(57>=57)
+
+#< operation
+# print(10<11)
+# print(11<13)
+# print(32<45)
+# print(11<88)
+# print(159<166)
+
+
+
+#<= operation
+# print(15<=21)
+# print(99<=100)
+# print(76<=81)
+# print(311<=311)
+# print(4.8<=24)
+
+#== operation
+# print(511==511)
+# print(199==199)
+# print(4.8==4.8)
+# print(3.9==3.9)
+# print(52.2==52.2)
+
+# num1 = (input("enter a number"))
+# num2 = (input("enter second number"))
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 >= num2)
+# print(num1 == num2)
+# print(num1 <=num2)
+
+#homework n8 done
+
+# num = int(input("enter a number"))
+# print(num>20 )
+# print(num < 30)
+# print(num >= 40)
+# print(num<= 35)
+# print(num == 115)
+
+#homework n9 done
+
+num1 = (input("enter a number:"))
+num2 = (input("enter second number:"))
+# print(num1 == num2)
+print(num1 != num2)
