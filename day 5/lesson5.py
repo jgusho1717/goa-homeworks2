c89351948e8d55a3a5a3fb1bf46b49c79af07fbe
jgusho1717 <rezijgushia17@gmail.com>
2024-03-15 19:47:26 +0400

    hw6

--- day 5/lesson5.py
+++ day 5/lesson5.py
@@ -1,3 +1,34 @@
-num1 = 10
-num2 = 15
-print(num1 + num2)
+# num1 = 10
+# num2 = 15
+# print(num1 + num2)
+
+
+num1 = int(input("please enter your first number:"))
+num2 = int(input("please enter your second number:"))
+
+# print(type(num1))
+# print(type(num2))
+
+# print(num1 / num2)
+# print (num1 // num2)
+# print(num1 % num2)
+
+#end of first part
+
+# print(type(num1))
+# print(type(num2))
+
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+
+#end of second part
+
+print(type(num1))
+print(type(num1))
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
