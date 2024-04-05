67deb947484319294cb2d05039460646edc0cf57
jgusho1717 <rezijgushia17@gmail.com>
2024-04-05 21:16:36 +0400

    commit

---
+++ day 9/day9hw.py
@@ -0,0 +1,12 @@
+num1 = int(input("please enter your salary: "))
+
+if num1 > 10000:
+    print("you studied in goa?")
+    #n1 done
+
+    if num1 ==1000:
+        print("you mid")
+    
+        
+else:
+        print("Enter Matricello in Goa")
