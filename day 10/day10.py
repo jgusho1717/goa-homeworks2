75d5bd8f791e9a8330c8e94d3a2dbd47d281f3eb
jgusho1717 <rezijgushia17@gmail.com>
2024-04-20 23:26:22 +0400

    day10

---
+++ day 10/day10.py
@@ -0,0 +1,51 @@
+# user= (input("In which programming academy do you study? "))
+# if user=="goa":
+#     print("good decision")
+# else:
+#     print("bad decision")
+#n1 done
+
+
+# num1 = int(input("enter a price of thing"))
+# num2 = int(input("enter your budget"))
+# if num2 >=num1:
+#     print("money is enough")
+# else:
+#     print("you need, num1- num2")
+#     n2 done
+
+
+# num = int(input("enter a num"))
+# if num>=5:
+#     print(num * 2)
+# else:
+#     print(num * 4)
+#n3 done
+
+# price = 10
+# num = float(input("How many tickets do you want to buy?"))
+# if num<5:
+#     print("its normal")
+# else:
+#     print(price - 10*0.3)
+#n4done
+  
+num1=int(input("enter a num"))
+num2=int(input("enter second num"))
+operation = input("please enter(+, -,*, /):")
+if operation == "+":
+    print(num1 +num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2 )
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("your operation is not correct")
+
+    
+
+
+
+
